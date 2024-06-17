6281d11a46ec318955c2260b3e5686544ce75e6c
Uli Bubenheimer <bubenheimer@users.noreply.github.com>
2024-06-17 10:14:40 -0400

    Make foreground service again

--- app/src/main/kotlin/org/bubenheimer/StartedService.kt
+++ app/src/main/kotlin/org/bubenheimer/StartedService.kt
@@ -1,17 +1,35 @@
 package org.bubenheimer
 
+import android.app.Notification
 import android.app.Service
 import android.content.ComponentName
 import android.content.Intent
 import android.content.ServiceConnection
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationCompat.CATEGORY_SERVICE
+import androidx.core.app.NotificationCompat.FOREGROUND_SERVICE_IMMEDIATE
+import org.bubenheimer.R.drawable.ic_stat_name
+import org.bubenheimer.R.id.notification_id_service
+import org.bubenheimer.R.string.channelid
 
 class StartedService : Service() {
     override fun onCreate() {
         Log.d("StartedService", "onCreate()")
 
         super.onCreate()
+
+        val notification: Notification =
+            NotificationCompat.Builder(this, getString(channelid))
+                .setContentTitle("Foreground service in progress")
+                .setCategory(CATEGORY_SERVICE)
+                .setOngoing(true)
+                .setSmallIcon(ic_stat_name)
+                .setForegroundServiceBehavior(FOREGROUND_SERVICE_IMMEDIATE)
+                .build()
+
+        startForeground(notification_id_service, notification)
     }
 
     override fun onDestroy() {
